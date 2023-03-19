1f91c774be5fdb73ed881584293b4730fd319138
slobodaapl <slobodaapl@gmail.com>
2023-03-19 10:55:48 +0100

    Reverting model changes

--- src/model/model.py
+++ src/model/model.py
@@ -97,8 +97,6 @@ class Generator(torch.nn.Module):
         self.interpretable_conv_2 = ConvBlock(num_filter, num_filter, kernel_size=1, stride=1, padding=0)
         self.interpretable_conv_3 = ConvBlock(num_filter, num_filter, kernel_size=1, stride=1, padding=0)
 
-        self.interpretable_reduce = ConvBlock(num_filter*2, 3, kernel_size=1, stride=1, padding=0)
-
         # Reflection padding
         self.pad = torch.nn.ReflectionPad2d(3)
         self.pad1 = torch.nn.ReflectionPad2d(1)
@@ -139,11 +137,8 @@ class Generator(torch.nn.Module):
             img = self.conv1dc(img)
             inv_masked_img = self.conv1dm(inv_masked_img)
 
-            inv_masked_img = self.interpretable_conv_1(inv_masked_img)
-            inv_masked_img = self.interpretable_conv_2(inv_masked_img)
-
         img = self.interpretable_conv_1(img)
-        img = self.interpretable_conv_2(img)
+        img = self.interpretable_conv_3(img)
 
         enc1 = self.conv1(self.pad(img))  # (bs, num_filter, 128, 128)
         enc2 = self.conv2(enc1)  # (bs, num_filter * 2, 64, 64)
@@ -164,7 +159,7 @@ class Generator(torch.nn.Module):
 
         if mask is not None:
             # noinspection PyUnboundLocalVariable
-            out = self.interpretable_reduce(torch.cat((out, inv_masked_img), 1))
+            out = out + inv_masked_img
 
         return out
 
